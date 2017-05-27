442abda5a3ee513a8d560e3795d0e23d7f71da5b
redes <estefanyval30@gmail.com>
2017-05-27 12:01:22 -0500

    first commit

---
+++ UVa11172/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n, a, b;
+  cin >> n;
+  while (n--){
+    cin >> a >> b;
+    if (a == b)
+      cout << '=';
+    else if (a > b)
+      cout << '>';
+    else
+      cout << '<';
+    cout << endl;
+  }
+}
